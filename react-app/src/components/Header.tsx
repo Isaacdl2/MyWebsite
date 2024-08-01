617dc27a2a295c6d7c70a5762d4b97f05ca25673
Isaac <isaaclarson42@gmail.com>
2024-07-31 22:34:05 -0700

    Website layout updates

--- react-app/src/components/Header.tsx
+++ react-app/src/components/Header.tsx
@@ -3,10 +3,22 @@ import "./Header.css";
 function Header() {
   return (
     <header className="header">
-      <div className="banner">
-        <h1>My Website</h1>
-        <p>Unorder list here</p>
-      </div>
+      <nav>
+        <ul className="header-nav">
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About Me</a>
+          </li>
+          <li>
+            <a href="#work">Work Experience</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
