c6b53c8da470ee6df3263f3f4a0c02833849e66e
Isaac <isaaclarson42@gmail.com>
2024-10-02 14:40:06 -0700

    stuff

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
   plugins: [react()],
   build: {
-    outDir: 'dist'
-  }
+    outDir: 'dist' // Ensure this is set to 'dist'
+  },
+
+  base: "/MyWebsite/",
 })
